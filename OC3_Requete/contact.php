9a9ccbcd48686a04f37c6cd815b0681c5366e246
tournesol59 <frederic.peugny@gmail.com>
2024-01-05 00:57:01 +0100

    openclassroom course
    Chapitre ecoutez la requete de vos utilisateurs

---
+++ OC3_Requete/contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Contact Form</title>
+</head>
+<body>
+<form action="submit_contact.php" method="GET">
+  <div>
+    <label for "email">Email</label>
+    <input type="email" name="email">
+  </div>
+  <div>
+    <label for="message">Your message</label>
+    <textarea placeholder="Express yourself" name="message"></textarea>
+  </div>
+  <button type="submit">Send</button>
+</form>
+</body>
+</html>
+

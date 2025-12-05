bcd1df7942319607b1f2a5ccbd6c1ebff5dc2c76
tournesol59 <frederic.peugny@gmail.com>
2025-12-05 00:40:45 +0000

    04 Dec 2025
    add a controller class contactrefctl.php, which is used in contactref.php

---
+++ CPP_Demo/contactrefctl.php
@@ -0,0 +1,66 @@
+<?php
+
+public class ContactRefCtl {
+
+   private string $db_name = "usercpp";
+   private string $db_user = "root";
+   private string $db_passwd = "";
+   public string $table = "user";
+   public string $contactname = "";
+
+// propriete qui contiendra l instance de connexion
+   protected $_connection;
+
+   public function __construct() : void {
+      // nothing
+   }
+
+   public function getConnection() {
+   // on supprime la connecxion precedente
+      $this->_connection = null;
+      try {
+         $this->_connection = new \PDO("mysql:host=" .$this->host .";dbname=". $this->db_name, $this->db_user, $this->db_passwd);
+
+         $this->_connection->exec("set names utf8");
+         $this->_connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $exception) {
+         echo "Erreur de connexion : ". $exception->getMessage();
+      }
+   }
+
+   public function findAll() : array {
+      $contacts=[];
+      try {
+         $sql = 'SELECT id, name, email, birthdate, useroption, contact, address FROM '. $table.' WHERE contact=true';
+         $query = $_connection->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+         $query->execute();
+         $contacts = $query->fetchAll();
+         return $contacts;
+
+      } catch (PDOException $exception) {
+         echo "Erreur de connexion : " .$exception->getMessage();
+         return [];
+      }
+   }
+
+   public function findByName($contactname) : array {
+      $contacts=[];
+      try {
+         $sql = 'SELECT id, name, email, birthdate, useroption, contact, address FROM '. $table .'WHERE name= :contactame';
+         $query = $_connection->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+         $query->execute(['contactname' => $contactname);
+         $contacts = $query->fetchAll();
+         if (isset($contacts)) {
+            return $contacts[0];  // return a unique result
+         }
+         else {
+            return [];
+         }
+      } catch (PDOException $exception) {
+         echo "Erreur de connexion : " .$exception->getMessage();
+         return [];
+      }
+   }
+
+
+?>
